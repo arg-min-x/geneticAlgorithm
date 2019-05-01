f52409b26a289cd51101bc4349d144f53f467aee
arg-min-x <rich.178@osu.edu>
2019-05-01 07:12:19 -0400

    filling in GA.py

--- GA.py
+++ GA.py
@@ -1,12 +1,14 @@
 #import keras
-from keras.datasets       import mnist
-from keras.models         import Model
-from keras.layers         import Dense, Dropout, Flatten, Input, Conv2D, MaxPooling2D, BatchNormalization, Activation
-
+from keras.datasets import mnist
+from keras.models import Model
+from keras.layers import Dense, Dropout, Flatten, Input, Conv2D, MaxPooling2D, BatchNormalization, Activation
+from keras.utils.np_utils import to_categorical
 import numpy as np
+import random
+
 
 class genticAlgorithm():
-    def __init__(self):
+    def __init__(self, input_shape, num_classes):
         self.input_shape = input_shape  # shape of the input layer to the netowork
         self.num_classes = num_classes  # number of classes to predict
         self.pop = []  # The current population of genes
@@ -34,47 +36,104 @@ class genticAlgorithm():
         :return:
         """
 
-class networkGenes:
-    def __init__(self,input_shape, num_layers, filters, kernel_size, output_size, dense_size):
-        self.input_shape = input_shape
-        self.num_layers = num_layers
-        self.filters = filters
-        self.kernel_size = kernel_size
-        self.output_size = output_size
-        self.dense_size = dense_size
+class networkGenesMnist:
+    def __init__(self):
+        self.input_shape = (28, 28, 1)
+        self.num_layers = []
+        self.filters = []
+        self.kernel_size = []
+        self.output_size = 10
+        self.dense_size = 32
 
+    def getRandomGenes(self):
+        """
+        :return:
+        """
+        self.num_layers = random.randint(1, 4)
+        for ii in range(self.num_layers):
+            self.filters.append(random.randint(4, 9))
+        self.kernel_size = random.randint(1, 5)
 
-def getNetwork(genes):
+    def mutateGene(self):
+        """
 
-    inputs = Input(shape=genes.input_shape)
-    for ii in range(genes.num_layers):
-        if ii == 1:
-            x = Conv2D(filters=genes.filters[ii], kernel_size=genes.kernel_size)(inputs)
-        else:
-            x = Conv2D(filters=genes.filters[ii], kernel_size=genes.kernel_size)(inputs)
-        x = BatchNormalization()(x)
-        x = Activation(activation='relu')(x)
-        x = MaxPooling2D()(x)
+        :return:
+        """
 
-    x = Flatten()(x)
-    x = Dense(genes.dense_size, activation='sigmoid')(x)
-    outputs = Dense(genes.output_size, activation='softmax')(x)
-    return inputs, outputs
+    def crossOverGenes(self, mom, dad):
+        """
 
+        :param mom:
+        :param dad:
+        :return:
+        """
 
-def getRandomGenes()
+class mnistGenetic:
+    def __init__(self):
+        """
 
+        """
 
-def testFitness(inputs, outputs, getData):
-    (nb_classes, batch_size, input_shape, x_train, x_test, y_train, y_test, epochs) = getData()
+    def getData(self):
+        """Retrieve the MNIST dataset and process the data."""
+        # Set defaults.
+        nb_classes = 10  # dataset dependent
+        batch_size = 64
+        epochs = 4
+        input_shape = (784,)
 
-    model = Model(inputs, outputs)
-    model.compile(optimizer='rmsprop',
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'])
+        # Get the data.
+        (x_train, y_train), (x_test, y_test) = mnist.load_data()
+        x_train = x_train.astype('float32')
+        x_test = x_test.astype('float32')
+        x_train /= 255
+        x_test /= 255
 
-    model.fit(x_train,
-              y_train,
-              validation_data=(x_test, y_test))  # starts training
+        x_train = np.reshape(x_train, x_train.shape + (1,))
+        x_test = np.reshape(x_test, x_test.shape + (1,))
 
-    return model.history.history['val_acc']
+        # convert class vectors to binary class matrices
+        y_train = to_categorical(y_train, nb_classes)
+        y_test = to_categorical(y_test, nb_classes)
+
+        return (nb_classes, batch_size, input_shape, x_train, x_test, y_train, y_test, epochs)
+
+    def getNetwork(self, genes):
+        """
+
+        :param genes:
+        :return:
+        """
+        inputs = Input(shape=genes.input_shape)
+        for ii in range(genes.num_layers):
+            if ii == 0:
+                x = Conv2D(filters=genes.filters[ii], kernel_size=genes.kernel_size)(inputs)
+            else:
+                x = Conv2D(filters=genes.filters[ii], kernel_size=genes.kernel_size)(x)
+            x = BatchNormalization()(x)
+            x = Activation(activation='relu')(x)
+            x = MaxPooling2D()(x)
+
+        x = Flatten()(x)
+        x = Dense(genes.dense_size, activation='sigmoid')(x)
+        outputs = Dense(genes.output_size, activation='softmax')(x)
+        return inputs, outputs
+
+    def testFitness(self, inputs, outputs):
+        """
+        :param inputs:
+        :param outputs:
+        :return:
+        """
+        (nb_classes, batch_size, input_shape, x_train, x_test, y_train, y_test, epochs) = self.getData()
+
+        model = Model(inputs, outputs)
+        model.compile(optimizer='rmsprop',
+                      loss='categorical_crossentropy',
+                      metrics=['accuracy'])
+
+        model.fit(x_train,
+                  y_train,
+                  validation_data=(x_test, y_test))  # starts training
+
+        return model.history.history['val_acc']
--- testAlg.py
+++ testAlg.py
@@ -7,48 +7,27 @@ from keras.callbacks      import EarlyStopping, Callback
 from keras                import backend as K
 import numpy as np
 import logging
-from GA import networkGenes, getNetwork, testFitness
-
-def get_mnist_mlp():
-    """Retrieve the MNIST dataset and process the data."""
-    # Set defaults.
-    nb_classes  = 10 #dataset dependent
-    batch_size  = 64
-    epochs      = 4
-    input_shape = (784,)
-
-    # Get the data.
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
-    x_train = x_train.astype('float32')
-    x_test  = x_test.astype('float32')
-    x_train /= 255
-    x_test  /= 255
-
-    x_train = np.reshape(x_train, x_train.shape + (1,))
-    x_test = np.reshape(x_test, x_test.shape + (1,))
-
-    # convert class vectors to binary class matrices
-    y_train = to_categorical(y_train, nb_classes)
-    y_test  = to_categorical(y_test, nb_classes)
-
-    return (nb_classes, batch_size, input_shape, x_train, x_test, y_train, y_test, epochs)
-
-(nb_classes, batch_size, input_shape, x_train, x_test, y_train, y_test, epochs) = get_mnist_mlp()
+from GA import *
 
 
-filters = [8, 8, 8]
-kernel_size = 2
-output_size = 10
-dense_size = 32
-genes = networkGenes(input_shape=(28, 28, 1),
-                     num_layers=3,
-                     filters=filters,
-                     kernel_size=kernel_size,
-                     output_size=10,
-                     dense_size=32)
-inputs, outputs = getNetwork(genes)
-
-accuracy = testFitness(inputs, outputs, get_mnist_mlp)
+# filters = [8, 8, 8]
+# kernel_size = 2
+# output_size = 10
+# dense_size = 32
+# genes = networkGenes(input_shape=(28, 28, 1),
+#                      num_layers=3,
+#                      filters=filters,
+#                      kernel_size=kernel_size,
+#                      output_size=10,
+#                      dense_size=32)
+genes = networkGenesMnist()
+genes.getRandomGenes()
+mnist_class = mnistGenetic()
+inputs, outputs = mnist_class.getNetwork(genes)
+mnist_class.testFitness(inputs, outputs)
+# inputs, outputs = getNetwork(genes)
+#
+# accuracy = testFitness(inputs, outputs, get_mnist_mlp)
 # model = Model(inputs, outputs)
 # model.compile(optimizer='rmsprop',
 #               loss='categorical_crossentropy',
