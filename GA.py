2a1d3e3f582100c4ebb5fc13d0a18fbe155e8ba8
arg-min-x <rich.178@osu.edu>
2019-05-05 19:22:07 -0400

    updates

--- GA.py
+++ GA.py
@@ -5,7 +5,10 @@ from keras.layers import Dense, Dropout, Flatten, Input, Conv2D, MaxPooling2D, B
 from keras.utils.np_utils import to_categorical
 import numpy as np
 import random
-
+from numpy.random import seed
+from tensorflow import set_random_seed
+set_random_seed(2)
+seed(1)
 
 class genticAlgorithm():
     def __init__(self, input_shape, num_classes, gene_class, network_class):
@@ -50,6 +53,8 @@ class genticAlgorithm():
         """
         num_kill = 1
 
+        num_mutate = 5
+
         # Get random genes to fill out the population
         for ii in range(pop_num):
             genes = self.gene_class()
@@ -57,10 +62,10 @@ class genticAlgorithm():
             self.pop.append(genes)
 
         # Initialize fitness
-        self.fitness = [-1 for ii in range(pop_num)]
+        self.fitness = [-1 for _ in range(pop_num)]
 
         # main loop
-        for ii in range(4):
+        for ii in range(5):
             # get the fitness of the population
             self.get_pop_fitness()
 
@@ -70,22 +75,58 @@ class genticAlgorithm():
             self.pop = [self.pop[ii] for ii in sort_inds]
             print(self.fitness)
 
-            # delete the lowest fitness genes and
-            self.fitness[-1] = -1
-            self.pop[-1].mutateGene()
+            # delete half  of the genes
+            del_genes = round(pop_num/2)
+
+            # flag fitness for retraining
+            for ii in range(del_genes, pop_num):
+                self.fitness[ii] = -1
+
+            breed_genes = self.pop[:del_genes]
+            del_genes_ind = list(range(del_genes, pop_num))
+            while breed_genes:
+                if len(breed_genes) == 1:
+                    self.pop[del_genes_ind[0]] = self.pop[0].mutateGene(breed_genes[0])
+                    breed_genes.pop(0)
+                    del_genes_ind.pop(0)
+                else:
+                    breed_genes_ind = random.choices(list(range(len(breed_genes))), k=2)
+                    brother, sister = crossOverGenes(self.pop[breed_genes_ind[0]], self.pop[breed_genes_ind[1]])
+                    self.pop[del_genes_ind[0]] = brother
+                    self.pop[del_genes_ind[1]] = sister
+
+                    breed_genes_ind.sort()
+                    breed_genes.pop(breed_genes_ind[0])
+                    breed_genes.pop(breed_genes_ind[1]-1)
+
+                    del_genes_ind.pop(0)
+                    del_genes_ind.pop(0)
+
+            # Mutate some genes
+            mutate_inds = np.random.choice(range(1, pop_num), size=num_mutate, replace=False)
+            for m_ind in mutate_inds:
+                mut_mult = random.randint(1, 2)
+                # mutate one gene
+                if mut_mult == 1:
+                    self.pop[m_ind] = self.pop[1].mutateGene(self.pop[m_ind])
+                # mutate two genes
+                else:
+                    self.pop[m_ind] = self.pop[1].mutateGene(self.pop[m_ind])
+                    self.pop[m_ind] = self.pop[1].mutateGene(self.pop[m_ind])
 
 
 class networkGenesMnist:
     def __init__(self):
         self.input_shape = (28, 28, 1)
         self.output_size = 10
-        self.dense_size = 32
         self.m_para = {'filters': [],
                        'kernel_size': [],
-                       'num_layers': []}
-        self.m_para_r = {'filters': (4, 9),
-                         'kernel_size': (1, 3),
-                         'num_layers': (1, 3)}
+                       'num_layers': [],
+                       'dense_size': []}
+        self.m_para_r = {'filters': (4, 16),
+                         'kernel_size': (1, 6),
+                         'num_layers': (1, 5),
+                         'dense_size': (16, 32)}
 
     def getRandomGenes(self):
         """
@@ -97,34 +138,79 @@ class networkGenesMnist:
                 random.randint(*self.m_para_r["filters"]))
         self.m_para["kernel_size"] = random.randint(
             *self.m_para_r["kernel_size"])
+        self.m_para["dense_size"] = random.randint(*self.m_para_r["dense_size"])
 
-    def mutateGene(self):
+    def mutateGene(self, parent, hash_table=None):
         """
 
         :return:
         """
-        # randomly chooses a gene to mutate
-        mut_gene = random.choice(list(self.m_para.keys()))
-        print(mut_gene)
-        print(self.m_para[mut_gene])
-        if mut_gene == 'filters':
-            self.m_para[mut_gene] = []
-            print(range(self.m_para_r["num_layers"][1]))
-            for ii in range(self.m_para_r["num_layers"][1]):
-                self.m_para[mut_gene].append(
-                    random.randint(*self.m_para_r[mut_gene]))
-        else:
-            self.m_para[mut_gene] = random.randint(*self.m_para_r[mut_gene])
-        print(self.m_para[mut_gene])
-        return 0
+        keep_mutating = 1
+        num_tries = 0
+        while keep_mutating:
+            child = parent
+            # randomly chooses a gene to mutate
+            mut_gene = random.choice(list(parent.m_para.keys()))
+            if mut_gene == 'filters':
+                child.m_para[mut_gene] = []
+                for ii in range(parent.m_para_r["num_layers"][1]):
+                    child.m_para[mut_gene].append(
+                        random.randint(*parent.m_para_r[mut_gene]))
+            else:
+                child.m_para[mut_gene] = random.randint(*parent.m_para_r[mut_gene])
 
-    def crossOverGenes(self, mom, dad):
+            # check if the gene was already tried
+            if child.hashGene() not in hash_table:
+                keep_mutating = 0
+            else:
+                num_tries += 1
+                print("duplicate gene")
+
+            # try 50 times, then give up
+            if num_tries > 50:
+                keep_mutating = 0
+
+        return child
+
+    def hashGene(self):
         """
-
-        :param mom:
-        :param dad:
         :return:
         """
+        # get all the values in the genes as  a list
+        gene_keys = list(self.m_para.keys())
+        genes = []
+        for keys in gene_keys:
+            if isinstance(self.m_para[keys], list):
+                for item in self.m_para[keys]:
+                    genes.append(item)
+            else:
+                genes.append(self.m_para[keys])
+
+        # convert to a string
+        genes = [str(item) for item in genes]
+        genes_str = ""
+        for gene_s in genes:
+            genes_str += gene_s
+        return genes_str
+
+
+def crossOverGenes(mom, dad):
+    """
+
+    :param mom:
+    :param dad:
+    :return:
+    """
+    brother = dad
+    sister = mom
+
+    # pick a random gene and cross over the parents
+    cross_gene = random.choice(list(mom.m_para.keys()))
+    mom_gene_tmp = mom.m_para[cross_gene]
+    sister.m_para[cross_gene] = dad.m_para[cross_gene]
+    brother.m_para[cross_gene] = mom_gene_tmp
+    return brother, sister
+
 
 class mnistGenetic:
     def __init__(self):
@@ -136,7 +222,7 @@ class mnistGenetic:
         """Retrieve the MNIST dataset and process the data."""
         # Set defaults.
         nb_classes = 10  # dataset dependent
-        batch_size = 64
+        batch_size = 128
         epochs = 4
         input_shape = (784,)
 
@@ -162,6 +248,10 @@ class mnistGenetic:
         :param genes:
         :return:
         """
+        # Set random seed
+        set_random_seed(2)
+        seed(1)
+
         inputs = Input(shape=genes.input_shape)
         for ii in range(genes.m_para["num_layers"]):
             if ii == 0:
@@ -172,10 +262,10 @@ class mnistGenetic:
                            kernel_size=genes.m_para["kernel_size"])(x)
             x = BatchNormalization()(x)
             x = Activation(activation='relu')(x)
-            x = MaxPooling2D()(x)
+            # x = MaxPooling2D()(x)
 
         x = Flatten()(x)
-        x = Dense(genes.dense_size, activation='sigmoid')(x)
+        x = Dense(genes.m_para["dense_size"], activation='sigmoid')(x)
         outputs = Dense(genes.output_size, activation='softmax')(x)
         return inputs, outputs
 
@@ -195,6 +285,7 @@ class mnistGenetic:
         model.fit(x_train,
                   y_train,
                   validation_data=(x_test, y_test),
-                  verbose=0)  # starts training
+                  verbose=0,
+                  epochs=2)  # starts training
 
         return model.history.history['val_acc'][0]
--- testAlg.py
+++ testAlg.py
@@ -1,44 +1,13 @@
-#import keras
-from keras.datasets       import mnist
-from keras.models         import Model
-from keras.layers         import Dense, Dropout, Flatten, Input, Conv2D, MaxPooling2D, BatchNormalization, Activation
-from keras.utils.np_utils import to_categorical
-from keras.callbacks      import EarlyStopping, Callback
-from keras                import backend as K
+
 import numpy as np
-import logging
 from GA import *
 
-ga = genticAlgorithm((28, 28), 10, networkGenesMnist, mnistGenetic)
-fitness = ga.main(pop_num=3)
 
-
-
-# filters = [8, 8, 8]
-# kernel_size = 2
-# output_size = 10
-# dense_size = 32
-# genes = networkGenes(input_shape=(28, 28, 1),
-#                      num_layers=3,
-#                      filters=filters,
-#                      kernel_size=kernel_size,
-#                      output_size=10,
-#                      dense_size=32)
-# genes = networkGenesMnist()
-# genes.getRandomGenes()
-# genes.mutateGene()
-# print(genes.m_para['filters'])
-# mnist_class = mnistGenetic()
-# inputs, outputs = mnist_class.getNetwork(genes)
-# mnist_class.testFitness(inputs, outputs)
-# inputs, outputs = getNetwork(genes)
-#
-# accuracy = testFitness(inputs, outputs, get_mnist_mlp)
-# model = Model(inputs, outputs)
-# model.compile(optimizer='rmsprop',
-#               loss='categorical_crossentropy',
-#               metrics=['accuracy'])
-#
-# model.fit(x_train,
-#           y_train,
-#           validation_data=(x_test, y_test))  # starts training
+genes_table = []
+for ii in range(5000):
+    gene = networkGenesMnist()
+    gene.getRandomGenes()
+    gene = gene.mutateGene(gene, hash_table=genes_table)
+    genes_table.append(gene.hashGene())
+# ga = genticAlgorithm((28, 28), 10, networkGenesMnist, mnistGenetic)
+# fitness = ga.main(pop_num=10)
